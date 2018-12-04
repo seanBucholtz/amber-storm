a078c833f845b3bf476ae3d7a5759756769c4f7f
Sean Bucholtz <sean.bucholtz@gmail.com>
2018-12-03 21:21:38 -0800

    init

---
+++ src/main/java/com/bucholtz/sean/automaticsummarization/AutomaticSummarizationApplication.java
@@ -0,0 +1,12 @@
+package com.bucholtz.sean.automaticsummarization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticSummarizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticSummarizationApplication.class, args);
+    }
+}
